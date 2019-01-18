ce40542d9ba7be06eadb2fb1479f3e89184f1006
Max Hully <max.hully@gmail.com>
2019-01-18 12:36:54 -0500

    Fix config

--- api/config.py
+++ api/config.py
@@ -2,7 +2,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = "postgresql://mggg:mgggiskool@localhost:5432/gis"
 
 try:
-    with open("/run/secrets/secret_key", "b") as f:
+    with open("/run/secrets/secret_key", "rb") as f:
         SECRET_KEY = f.read()
 except FileNotFoundError:
+    print("Using development secret key. Never do this in production!")
     SECRET_KEY = "my_secret_key"
