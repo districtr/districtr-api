f9a64f04819cf439a0cc88e7e625bab4c82de15d
Max Hully <max.hully@gmail.com>
2019-01-18 19:10:23 -0500

    Re-do database migration

--- up.sh
+++ up.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
-# pipenv run flask db init
-# pipenv run flask db upgrade
+# Do the database migration. We do a retry loop in case we failed due to
+# the database not being ready.
+while true; do
+    pipenv run flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo "Upgrade command failed, retrying in 5 secs..."
+    sleep 5
+done
+
 exec pipenv run gunicorn -b :3000 --access-logfile - --error-logfile - "api:create_app()"
