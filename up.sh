441d6d40a89fedbbcb442bf3cc7b9f0041e852b8
Max Hully <max.hully@gmail.com>
2019-01-18 19:35:43 -0500

    Don't bother with retry loop

--- up.sh
+++ up.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-# Do the database migration. We do a retry loop in case we failed due to
-# the database not being ready.
-while true; do
-    pipenv run flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo "Upgrade command failed, retrying in 5 secs..."
-    sleep 5
-done
+pipenv run flask db upgrade
 
 exec pipenv run gunicorn -b :3000 --access-logfile - --error-logfile - "api:create_app()"
