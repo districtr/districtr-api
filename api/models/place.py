6c2391f53b5d66c60c307ceda51b83845c6e9a18
Max Hully <max.hully@gmail.com>
2019-01-21 17:11:00 -0500

    Add columns to places

--- api/models/place.py
+++ api/models/place.py
@@ -1,11 +1,24 @@
 from .db import db
 
 
+class Column(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    key = db.Column(db.String(80), nullable=False)
+    min = db.Column(db.Float())
+    max = db.Column(db.Float())
+    sum = db.Column(db.Float())
+    is_id_column = db.Column(db.Boolean(), default=False)
+
+
 class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text)
 
+    id_column_id = db.Column(db.Integer, db.ForeignKey("column.id"))
+
+    columns = db.relationship("Column", backref="place", lazy=True)
     plans = db.relationship("Plan", backref="place")
 
     def __repr__(self):
--- api/schemas/place.py
+++ api/schemas/place.py
@@ -1,11 +1,25 @@
 from marshmallow import Schema, fields, post_load
 
 from ..models import Place
+from ..models.place import Column
+
+
+class ColumnSchema(Schema):
+    name = fields.Str(required=True)
+    key = fields.Str(required=True)
+    min = fields.Float()
+    max = fields.Float()
+    sum = fields.Float()
+    is_id = fields.Boolean(default=False)
+
+    @post_load
+    def create_column(self, data):
+        return Column(**data)
 
 
 class PlaceSchema(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str()
+    name = fields.Str(required=True)
     description = fields.Str()
 
     @post_load
