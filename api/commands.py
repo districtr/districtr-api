3e6a653c3708a0d2bc9feea79b6fb5f145e1a7bb
Max Hully <max.hully@gmail.com>
2019-04-02 10:31:54 -0400

    Refactor roles cli

--- api/commands.py
+++ api/commands.py
@@ -2,16 +2,21 @@ from .models import User, db
 import click
 from flask.cli import AppGroup
 
-admin_cli = AppGroup("admin")
+admin_cli = AppGroup("roles")
 
 
-@admin_cli.command("create")
-@click.argument("email")
-def make_admin(email):
+@admin_cli.command("add")
+@click.option("--email")
+@click.argument("role")
+def add_role(role, email):
     user = User.by_email(email)
     if not user:
         click.echo("The specified user does not exist.")
-        return
-    if not user.has_role("admin"):
-        user.add_role("admin")
+        click.exit(1)
+    try:
+        user.add_role(role)
         db.session.commit()
+    except ValueError:
+        click.echo("The specified role is not defined.")
+        click.exit(1)
+    click.echo("User {} now has role {}.".format(email, role))
