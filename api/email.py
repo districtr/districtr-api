1dd73c861b77ca0fae09fa871b5e90c36974b2f1
Max Hully <max.hully@gmail.com>
2019-04-16 12:05:17 -0400

    Fix SendGridAPIClient instantiation

--- api/email.py
+++ api/email.py
@@ -17,7 +17,7 @@ def send_email(from_address, to_address, subject, body):
         print("Sending mail", mail)
         return
 
-    client = SendGridAPIClient(apikey=current_app.config["SENDGRID_API_KEY"]).client
+    client = SendGridAPIClient(api_key=current_app.config["SENDGRID_API_KEY"]).client
     response = client.mail.send.post(request_body=mail.get())
 
     if response.status_code >= 300:
